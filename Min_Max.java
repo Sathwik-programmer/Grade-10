3889f8f49c8cd41d7022ccaeb913bed9d383643c
Sathwik <tsathwikreddy7@gmail.com>
2026-03-03 21:46:58 +0530

    commit

--- Min_Max.java
+++ Min_Max.java
@@ -1,15 +1,27 @@
+import java.util.*;
 public class Min_Max {
-    public static void main(String[] args) {
-        int[] arr = {5, 2, 9, 1, 5, 6};
+    public static void main(String[] args) 
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = in.nextInt();
+        int[]arr = new int[n];
+        System.out.print("Enter the elements: ");
+        for (int i=0; i<n; i++) 
+            {
+            arr[i] = in.nextInt();
+        }
         int min = arr[0];
         int max = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
+            if (arr[i] <min) 
+                {
+                min=arr[i];
             }
-            if (arr[i] > max) {
-                max = arr[i];
+            if (arr[i]>max) 
+                {
+                max=arr[i];
             }
         }
 
